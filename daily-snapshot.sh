a3b2355d1397b0f296b25a6002013ae6dae8ece7
Tyler <45154554+santost12@users.noreply.github.com>
2022-02-04 21:02:02 -0500

    Create daily-snapshot.sh

---
+++ daily-snapshot.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+###
+# This script makes a lot of assumptions!
+# 1) Assumes your /etc/fstab is setup!
+# 2) Assumes you keep your data in /data/ subvolume.
+# 3) Assumes you keep your snapshots in /snapshots/ subvolume.
+# 4) You run it once a day at most.
+###
+
+# mount points for each of the drives
+mountPoints=(/mnt/hdd/ /mnt/oldhdd/)
+
+# I'm using year-month-day format. Example: 2022-1-20
+currentDate=$(date +%y-%m-%d)
+
+dataSubVolId="256"
+
+
+function unmountAll()
+{
+	for mnt in "${mountPoints[@]}";
+		do sudo umount "$mnt";
+	done
+}
+
+
+function mountRootOfDrives()
+{
+	for mnt in "${mountPoints[@]}";
+		do sudo mount "$mnt" -o noatime,subvolid=0;
+	done
+}
+
+
+function snapshot()
+{
+	for mnt in "${mountPoints[@]}";
+		do sudo btrfs subvolume snapshot "$mnt"data/ "$mnt"snapshots/"$currentDate";
+	done
+}
+
+
+function regularMount()
+{
+	for mnt in "${mountPoints[@]}";
+		do sudo mount "$mnt";
+	done
+}
+
+
+unmountAll
+
+mountRootOfDrives
+
+snapshot
+
+unmountAll
+
+regularMount
